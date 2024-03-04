ebca44e1a329324921256a30483e879c6c778a16
Herman Brunborg <herman@brunborg.com>
2024-03-04 12:12:26 -0800

    evaluate jupyter cell blocks in jupytext format

--- plugin/magma.lua
+++ plugin/magma.lua
@@ -6,3 +6,32 @@ function MagmaVisualSendLines(mode)
 
     vim.api.nvim_command('MagmaVisualSend '.. start_pos[1] .. ',' .. end_pos[1])
 end
+
+function EvaluateCodeBlock(skipToNextCodeBlock)
+    local original_cursor = vim.api.nvim_win_get_cursor(0)
+    local mode = vim.api.nvim_get_mode().mode
+    if mode == 'i' or mode == 'v' or mode == 'V' or mode == '' then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+    end
+    local current_line = vim.api.nvim_get_current_line()
+    if current_line == "# +" or current_line == "# -" then
+        vim.cmd('normal! j')
+    end
+    local pattern = '# [-+]$'
+    local start_row = vim.fn.search(pattern, 'bnW') + 1
+    local end_row = vim.fn.search(pattern, 'nW') - 1
+    if end_row == -1 then end_row = vim.fn.line('$') end
+    if start_row > end_row then return end
+
+    vim.api.nvim_command('MagmaVisualSend '.. start_row .. ',' .. end_row)
+
+    if skipToNextCodeBlock and end_row == vim.fn.line('$') then
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, {"# +"})
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
+    elseif end_row == vim.fn.line('$') - 1 then
+        vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
+    end
+    if skipToNextCodeBlock then
+        vim.api.nvim_win_set_cursor(0, {end_row + 2, 0})
+    end
+end
